235350cfa571fe3f2c2f2a3c81f1baecb53658b1
sdsurya535 <145953785+sdsurya535@users.noreply.github.com>
2024-05-22 09:36:44 +0530

    Delete StManagementSystem directory

--- StManagementSystem/smsbackend/src/main/java/com/smsb/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.smsb.dao;
-
-import com.smsb.entities.User;
-
-public interface UserDao {
-	
-	boolean registerUser(User user);
-	User getUserByEmailAndPassword(String email, String password);
-}
